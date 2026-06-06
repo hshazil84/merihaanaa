2755e43b596a5d692d7fa023d0738f93f88e2e27
hshazil84 <hshazil@gmail.com>
2026-06-06 10:17:12 +0500

    Update MainWrapper.tsx

--- src/components/public/MainWrapper.tsx
+++ src/components/public/MainWrapper.tsx
@@ -4,9 +4,7 @@ export default function MainWrapper({ children }: { children: React.ReactNode })
   const pathname = usePathname();
   const isHome = pathname === "/";
   return (
-    <main style={{ paddingTop: isHome ? "0" : "128px" }}
-      className={isHome ? "" : "md:pt-[128px] pt-[72px]"}
-    >
+    <main className={isHome ? "" : "pt-[56px] md:pt-[128px]"}>
       {children}
     </main>
   );
