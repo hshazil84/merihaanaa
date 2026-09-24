28a35c5086888446b325c3f498e570160050cac0
hshazil84 <hshazil@gmail.com>
2026-09-24 18:09:25 +0500

    Update PublicFooter.tsx

--- src/components/public/PublicFooter.tsx
+++ src/components/public/PublicFooter.tsx
@@ -75,7 +75,7 @@ export default function PublicFooter({ categories }: { categories: Category[] })
               <path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-2.88 2.5 2.89 2.89 0 0 1-2.89-2.89 2.89 2.89 0 0 1 2.89-2.89c.28 0 .54.04.79.1V9.01a6.32 6.32 0 0 0-.79-.05 6.34 6.34 0 0 0-6.34 6.34 6.34 6.34 0 0 0 6.34 6.34 6.34 6.34 0 0 0 6.33-6.34V8.69a8.16 8.16 0 0 0 4.77 1.52V6.75a4.85 4.85 0 0 1-1-.06z"/>
             </svg>
           </a>
-          <a href="https://facebook.com/merihaanaa" target="_blank" rel="noopener noreferrer" aria-label="Facebook"
+          <a href="https://www.facebook.com/Merihaanaadotcom/" target="_blank" rel="noopener noreferrer" aria-label="Facebook"
             className="transition-opacity hover:opacity-60" style={{ color: "rgba(255,255,255,0.4)" }}>
             <svg width="16" height="16" viewBox="0 0 24 24" fill="currentColor">
               <path d="M24 12.073C24 5.405 18.627 0 12 0S0 5.405 0 12.073C0 18.1 4.388 23.094 10.125 24v-8.437H7.078v-3.49h3.047V9.41c0-3.025 1.792-4.697 4.533-4.697 1.312 0 2.686.236 2.686.236v2.97h-1.513c-1.491 0-1.956.93-1.956 1.874v2.25h3.328l-.532 3.49h-2.796V24C19.612 23.094 24 18.1 24 12.073z"/>
